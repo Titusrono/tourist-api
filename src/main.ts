21e9f25b88e13f6399c8d7bfe9c4bdadeaa5f59f
Titusrono <tituskrono04@gmail.com>
2024-05-17 22:12:13 +0300

    vercel set up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
+
+const corsOptions:CorsOptions = {
+  origin: '*',
+  methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}
+
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
